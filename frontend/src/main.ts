52f1914b451a45604ca86f8f55c76793c07547b2
Benjamin Ebo Yankson <33305846+Qwabhina@users.noreply.github.com>
2026-03-28 17:34:19 +0000

    refactor: update build assets and simplify main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,17 +5,12 @@ import { createPinia } from 'pinia'
 
 import { injectCSSVars } from "@/design-system";
 import "@/design-system/styles/base.css";
-import "@/design-system/styles/animations.css";
 
 import App from './App.vue'
 import router from './router'
 
-injectCSSVars({
-   '--ch-color-primary': '#00026D',
-   '--ch-color-primary-hover': '#0003ae',
-   '--ch-color-primary-active': '#0004ed',
-   '--ch-color-primary-contrast': '#ffffff',
-});
+injectCSSVars();
+
 const app = createApp(App)
 
 app.use(createPinia())
